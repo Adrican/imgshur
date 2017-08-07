9af68325984bff5909a772b9850d7e8eb1af38fa
Adrian <adriancan21@gmail.com>
2017-08-07 22:26:59 +0200

    imgshur inicio

---
+++ src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Camera } from '@ionic-native/camera';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+
+
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { ImagePicker } from '@ionic-native/image-picker';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB-dS_BwzFNGTgZ1HDmlokGgis4i6GUlnE",
+    authDomain: "imgshur.firebaseapp.com",
+    databaseURL: "https://imgshur.firebaseio.com",
+    projectId: "imgshur",
+    storageBucket: "imgshur.appspot.com",
+    messagingSenderId: "449261701468"
+  };
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    ImagePicker,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Camera,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,99 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import 'firebase/storage';
+import * as firebase from 'firebase/app'; // for typings
+import { FirebaseApp } from 'angularfire2'; // for methods
+
+import { ToastController } from 'ionic-angular';
+import { ImagePicker } from '@ionic-native/image-picker';
+
+
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+export class HomePage {
+
+  items: FirebaseListObservable<any[]>;
+
+  constructor(public navCtrl: NavController, private toastCtrl: ToastController, private imagePicker: ImagePicker, db: AngularFireDatabase, private Camera: Camera, private firebase: FirebaseApp) {
+    this.items = db.list('/items');
+
+
+  
+  }
+
+  copiedToast() {
+    let toast = this.toastCtrl.create({
+      message: 'Link copiado',
+      duration: 1500,
+      position: 'bottom'
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
+  }
+
+  
+
+  cogerImagen(){
+      let options = {
+        maximumImagesCount: 1,
+        width: 300,
+        height: 300,
+        quality : 75
+      };
+      this.imagePicker.getPictures(options).then((results) => {
+      for (var i = 0; i < results.length; i++) {
+          console.log('Image URI: ' + results[i]);
+      }
+    }, (err) => { });
+  }
+  
+captureDataUrl: string;
+
+hacerFoto() {
+    const cameraOptions: CameraOptions = {
+      quality: 50,
+      destinationType: this.Camera.DestinationType.DATA_URL,
+      encodingType: this.Camera.EncodingType.JPEG,
+      mediaType: this.Camera.MediaType.PICTURE,
+    };
+
+    this.Camera.getPicture(cameraOptions).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      this.captureDataUrl = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
+  }
+
+  subirImagen() {
+    let storageRef = firebase.storage().ref();
+    // Create a timestamp as filename
+    const filename = Math.floor(Date.now() / 1000);
+
+    // Create a reference to 'images/todays-date.jpg'
+    const imageRef = storageRef.child(`images/${filename}.jpg`);
+
+    imageRef.putString(this.captureDataUrl, firebase.storage.StringFormat.DATA_URL).then((snapshot)=> {
+     // Do something here when the data is succesfully uploaded!
+    });
+
+  }
+
+}
+
+
